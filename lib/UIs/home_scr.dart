f3503400e28fa6aba4c4301c917657ede99ce3a0
ahmed Mosaad <ahmed.mosaad2018@gmail.com>
2020-09-17 14:50:35 +0200

    this is changes

--- lib/UIs/home_scr.dart
+++ lib/UIs/home_scr.dart
@@ -2,32 +2,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:keep/UIs/note_page.dart';
 import 'package:keep/repo/repo.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery
-        .of(context)
-        .size;
+    Size size = MediaQuery.of(context).size;
+
+
+
 
     return Scaffold(
       drawer: Container(
         height: size.height,
-        width: size.width / 1.5,
+        width: size.width/1.4,
         color: Colors.white,
       ),
       body: CustomScrollView(
         slivers: [
 
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: MediaQuery
-                  .of(context)
-                  .padding
-                  .top,
-            ),
-          ),
           SliverPadding(
             padding: EdgeInsets.all(10),
             sliver: SliverAppBar(
@@ -46,7 +40,9 @@ class Home extends StatelessWidget {
               actions: [
                 IconButton(icon: Icon(Icons.view_list), onPressed: () {}),
                 CircleAvatar(),
-                SizedBox(width: 5,)
+                SizedBox(
+                  width: 5,
+                )
               ],
             ),
           ),
@@ -56,27 +52,20 @@ class Home extends StatelessWidget {
               child: Text('PINNED', style: TextStyle(fontSize: 12),),
             ),
           ),
+
           SliverPadding(
             padding: EdgeInsets.all(10),
-            sliver: SliverToBoxAdapter(
-              child: SizedBox(
-                child: StaggeredGridView.countBuilder(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  crossAxisCount: 4,
-                  itemCount: ListRepo().pins.length,
-                  itemBuilder: (BuildContext context, i){
-                    return HomeNoteBox(
-                      title: ListRepo().pins[i].title,
-                      note: ListRepo().pins[i].note,
-                    );
-                  },
-                  staggeredTileBuilder: (int index) =>
-                  new StaggeredTile.fit(2),
-                  mainAxisSpacing: 4.0,
-                  crossAxisSpacing: 4.0,
-                ),
+            sliver: SliverStaggeredGrid.countBuilder(
+              crossAxisCount: 2,
+              itemCount: ListRepo().pins.length,
+              itemBuilder: (BuildContext context, int index) => HomeNoteBox(
+                title: ListRepo().pins[index].title,
+                note: ListRepo().pins[index].note,
+                i: index,
               ),
+              staggeredTileBuilder: (int index) => new StaggeredTile.fit(1),
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
             ),
           ),
           SliverPadding(
@@ -85,29 +74,24 @@ class Home extends StatelessWidget {
               child: Text('OTHERS', style: TextStyle(fontSize: 12),),
             ),
           ),
+
           SliverPadding(
             padding: EdgeInsets.all(10),
-            sliver: SliverToBoxAdapter(
-              child: SizedBox(
-                child: StaggeredGridView.countBuilder(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  crossAxisCount: 4,
-                  itemCount: ListRepo().general.length,
-                  itemBuilder: (BuildContext context, i){
-                    return HomeNoteBox(
-                      title: ListRepo().general[i].title,
-                      note: ListRepo().general[i].note,
-                    );
-                  },
-                  staggeredTileBuilder: (int index) =>
-                  new StaggeredTile.fit(2),
-                  mainAxisSpacing: 4.0,
-                  crossAxisSpacing: 4.0,
-                ),
+            sliver: SliverStaggeredGrid.countBuilder(
+              crossAxisCount: 2,
+              itemCount: ListRepo().general.length,
+              itemBuilder: (BuildContext context, int index) => HomeNoteBox(
+                title: ListRepo().general[index].title,
+                note: ListRepo().general[index].note,
+                i: index,
               ),
+              staggeredTileBuilder: (int index) => new StaggeredTile.fit(1),
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+
             ),
           ),
+
         ],
       ),
     );
@@ -115,35 +99,52 @@ class Home extends StatelessWidget {
 }
 
 class HomeNoteBox extends StatelessWidget {
-
   final String title;
   final String note;
-  const HomeNoteBox({
-    this.title, this.note
-  });
+  final int i;
+
+  const HomeNoteBox({this.title, this.note, this.i});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(6),
-          border: Border.all(
-              color: Colors.grey.withOpacity(.3)
-          )
-      ),
-      padding: EdgeInsets.all(10),
-
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(title, maxLines: 1,
-            style: TextStyle(
-                fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black.withOpacity(.5)),
-            overflow: TextOverflow.ellipsis,),
-          Divider(),
-          Text(note, style: TextStyle(fontSize: 14, color: Colors.black.withOpacity(.7),), maxLines: 10,)
-        ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => NotePage(
+          index: i,
+        )));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+            border: Border.all(color: Colors.grey.withOpacity(.3))),
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              maxLines: 1,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black.withOpacity(.5)),
+              overflow: TextOverflow.ellipsis,
+            ),
+            Divider(),
+            Text(
+              note,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black.withOpacity(.7),
+              ),
+              maxLines: 10,
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+
--- lib/UIs/note_page.dart
+++ lib/UIs/note_page.dart
@@ -1,7 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:keep/repo/repo.dart';
 
-class  extends StatelessWidget {
+class NotePage extends StatefulWidget {
+
+ final int index;
+
+  const NotePage({ this.index});
+
+  @override
+  _NotePageState createState() => _NotePageState();
+}
+
+class _NotePageState extends State<NotePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    TextEditingController _titleControl = TextEditingController(
+      text: ListRepo().pins[widget.index].title
+    );
+    TextEditingController _noteControl = TextEditingController(
+        text: ListRepo().pins[widget.index].note
+    );
+
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(icon: Icon(FontAwesomeIcons.thumbtack), onPressed: () {}),
+          IconButton(icon: Icon(Icons.add_alert), onPressed: () {}),
+          IconButton(icon: Icon(FontAwesomeIcons.archive), onPressed: () {}),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _titleControl,
+                  decoration: InputDecoration(
+                      hintText: 'Title', border: InputBorder.none),
+                ),
+                Expanded(
+                    child: TextFormField(
+                  controller: _noteControl,
+                      style: TextStyle(fontSize: 16,),
+                  expands: true,
+                  minLines: null,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    hintText: 'Note',
+                    border: InputBorder.none,
+                  ),
+
+                ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
